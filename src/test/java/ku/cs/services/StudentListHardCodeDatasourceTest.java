5b7d10756b0654d36265e897e5d6d8dd15be93cf
thanakrit muangsuk <ThanakritMuangsuk@gmail.com>
2025-08-27 20:46:09 +0700

    StudentListHardCodeDatasourceTest finish

---
+++ src/test/java/ku/cs/services/StudentListHardCodeDatasourceTest.java
@@ -0,0 +1,28 @@
+package ku.cs.services;
+
+import ku.cs.models.Student;
+import ku.cs.models.StudentList;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentListHardCodeDatasourceTest {
+    @Test
+    @DisplayName("ทดสอบ readData")
+    void readDataTest()
+    {
+        StudentListHardCodeDatasource datasource = new StudentListHardCodeDatasource();
+        StudentList l = datasource.readData();
+
+        int i = 0;
+        for (Student s: l.getStudents()) {//ไล่ทดสอบ StudentList l ทีละตัว
+            assertEquals(s.getId(), l.getStudents().get(i).getId());//ทดสอบ ID ว่าเหมือนกันหรือไม่
+            assertEquals(s.getName(),l.getStudents().get(i).getName());//ทดสอบชื่อว่าเหมือนกันหรือไม่
+            assertEquals(s.getScore(),l.getStudents().get(i).getScore());//ทดสอบคะแนนว่าเท่ากันหรือไม่
+            i++;
+        }
+    }
+
+    //ไม่มี method writeData
+}
